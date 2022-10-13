1553091a0e5633b61c07800bc25e7916bc2f17a7
ibrahim <ibrahimmutasim10@gmail.com>
2022-10-13 15:10:25 -0700

    vl,veeee

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
  * main - Entry
  * Return: Always
  */
-int mane(void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of artwith proper grammar, but the outcome is a piece of art\n");
 	return (0);
