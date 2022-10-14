a9cca025dcc539cf818015367292353e6f236765
ibrahim <ibrahimmutasim10@gmail.com>
2022-10-14 06:36:47 -0700

    jfnffl

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,6 +5,24 @@
  */
 int main(void)
 {
-	printf("Size of a char: %i byte\n"); sizeo
-		f(char)
-		printf("Size of a char: %i byte\n"); sizeo 
+	int intType;
+
+	    float floatType;
+
+	        double doubleType;
+
+		    char charType;
+
+
+
+		        /* sizeof evaluates the size of a variable*/
+			
+			     printf("Size of int: %zu bytes\n", sizeof(intType));
+		
+			         printf("Size of float: %zu bytes\n", sizeof(floatType));
+			
+			             printf("Size of double: %zu bytes\n", sizeof(doubleType));
+			
+			                 printf("Size of char: %zu byte\n", sizeof(charType));
+			                         return 0;
+	}
